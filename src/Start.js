3243127f8c2ef92585534957cbea4900e00ae8d7
Geo Min <31418725+geomin76@users.noreply.github.com>
2021-11-04 15:41:04 -0400

    Init (#1)
    * adding todo
    
    * organizing

---
+++ src/Start.js
@@ -0,0 +1,68 @@
+import { Container, Box, Input, Button } from '@mui/material';
+import { useState } from 'react';
+import "./App.css"
+import { useHistory } from "react-router";
+
+export const Start = ({file, setFile, setArtwork, setYear, setName}) => {
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+      setFile(URL.createObjectURL(event.target.files[0]))
+    }
+
+    const handleArtworkChange = (event) => {
+        setArtwork(event.target.value);
+    }
+
+    const handleYearChange = (event) => {
+        setYear(event.target.value);
+    }
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+    
+    /**
+     * TODO:
+     * Add type of painting (randomized), Acrylic on canvas, oil, etc
+     * Add where the painting came from (randomized)
+     * Also add the year of created
+     * 
+     * Image CSS organization
+     * Add picture frame around the image
+     * Add the "paper-like" museum portion
+     * Figure out ratios and where to put paper item
+     * Downloadable image
+     * 
+     * Pretty it up
+     * Transitions and animations
+     */
+  
+    return (
+      <Container maxWidth="sm" >
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <h1 onClick={() => history.push("/")}>Put it in the Louvre</h1>
+        </Box>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Input type="file" onChange={handleChange}/>
+        </Box>
+        <Box display="flex" justifyContent="center" alignItems="center" paddingTop={2}>
+          <img src={file} alt="" width="100%"/>
+  
+        </Box>
+        <Box paddingTop={2} paddingBottom={2}>
+         <Input type="text" onChange={handleArtworkChange} placeholder="Name of the artwork" fullWidth/>
+        </Box>
+        <Box paddingTop={2} paddingBottom={2}>
+          <Input type="text" onChange={handleYearChange} placeholder="Year the artist was born (optional)" fullWidth/>
+        </Box>
+        <Box paddingTop={2} paddingBottom={2}>
+          <Input type="text" onChange={handleNameChange} placeholder="Name of the artist" fullWidth />
+        </Box>
+        <Box paddingTop={2} paddingBottom={2} display="flex" justifyContent="center" alignItems="center">
+          <Button onClick={() => history.push("/art")} variant="outlined">Put it in the Louvre!</Button>
+        </Box>
+      </Container>
+    );
+}
