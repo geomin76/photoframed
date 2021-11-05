278a36ed458294a9f588b5f19737c7456cefff21
geomin76 <geom@vt.edu>
2021-11-05 15:25:51 -0400

    cleanup

--- src/Start.js
+++ src/Start.js
@@ -39,29 +39,36 @@ export const Start = ({file, name, artwork, setFile, setArtwork, setYear, setNam
   
     return (
       <Container maxWidth="sm" >
+
         <Box display="flex" justifyContent="center" alignItems="center">
           <h1 onClick={() => history.push("/")}>Put it in the Louvre</h1>
         </Box>
+
         <Box display="flex" justifyContent="center" alignItems="center">
           <Input type="file" onChange={handleChange}/>
         </Box>
+
         <Box display="flex" justifyContent="center" alignItems="center" paddingTop={2}>
           <img src={file} alt="" width="100%"/>
-  
         </Box>
+
         <Box paddingTop={2} paddingBottom={2}>
          <Input type="text" onChange={handleArtworkChange} placeholder="Name of the artwork" fullWidth/>
         </Box>
+        
         <Box paddingTop={2} paddingBottom={2}>
           <Input type="text" onChange={handleYearChange} placeholder="Year the artist was born (optional)" fullWidth/>
         </Box>
+
         <Box paddingTop={2} paddingBottom={2}>
           <Input type="text" onChange={handleNameChange} placeholder="Name of the artist" fullWidth />
         </Box>
+
         <Box paddingTop={2} paddingBottom={2} display="flex" justifyContent="center" alignItems="center">
           { (artwork !== "" && name !== "" && file !== null) && <Button onClick={() => history.push("/art")} variant="outlined">Put it in the Louvre!</Button> }
           { (artwork === "" || name === "" || file === null) && <Button disabled variant="outlined">Put it in the Louvre!</Button> }
         </Box>
+        
       </Container>
     );
 }
