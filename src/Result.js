cbf1e7396cb6b83096940e5a9cb88f07570aebdd
geomin76 <geom@vt.edu>
2021-12-05 20:41:33 -0500

    update

--- src/Result.js
+++ src/Result.js
@@ -13,15 +13,15 @@ export const Result = ({ file, artwork, year, name, setFile }) => {
                 <h1 onClick={() => {
                     history.push("/")
                     setFile(null);
-                }}>Put it in the Louvre</h1>
+                }}>photoframed</h1>
             </Box>
             <Grid container>
-                <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+                <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
                     <div className="frame">
                         <img src={file} alt=""/>
                     </div>
                 </Grid>
-                <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
                     <Box sx={{
                         display: 'flex',
                         flexWrap: 'wrap',
