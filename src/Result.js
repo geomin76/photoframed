f5264cb3c25886e0fd768624d95e151a4a5983ff
Geo Min <31418725+geomin76@users.noreply.github.com>
2021-12-05 21:20:01 -0500

    adding (#8)

--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,38 @@
-import { Container, Box, Paper, Grid } from '@mui/material';
+import { Container, Box, Paper, Grid, Button } from '@mui/material';
 import { useHistory } from 'react-router';
 import { PaintingType } from './Helper';
 import './App.css'
+import React from 'react';
+import html2canvas from "html2canvas";
 
 export const Result = ({ file, artwork, year, name, setFile }) => {
 
     const history = useHistory();
+
+    const printRef = React.useRef();
+
+    const handleDownloadImage = async () => {
+        const element = printRef.current
+        const canvas = await html2canvas(element, {
+          windowWidth: '1280px',
+          useCORS: true,
+          scale: 4
+        });
+     
+        const data = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+     
+        if (typeof link.download === 'string') {
+          link.href = data;
+          link.download = 'image.jpg';
+     
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        } else {
+          window.open(data);
+        }
+      };
 
     return (
         <Container maxWidth="xl">
@@ -15,36 +42,46 @@ export const Result = ({ file, artwork, year, name, setFile }) => {
                     setFile(null);
                 }}>photoframed</h1>
             </Box>
-            <Grid container>
-                <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
-                    <div className="frame">
-                        <img src={file} alt=""/>
-                    </div>
+            <div ref={printRef}>
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
+                        <div className="frame">
+                            <img src={file} alt=""/>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                        <Box sx={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            '& > :not(style)': {
+                            m: 4,
+                            width: 425,
+                            height: 250,
+                            },
+                        }}>
+                            <Paper elevation={3}>
+                                <Box paddingLeft={3}>
+                                    <p style={{ fontSize: "22px", marginBottom: 5 }}><b>{name}</b> {year !== "" && <>(b. {year})</>}</p>
+                                </Box>
+                                <Box paddingLeft={3}>
+                                    <p style={{ fontSize: "22px", paddingTop: 3, margin: 0 }}><i><b>{artwork}</b></i>, 2021</p>
+                                </Box>
+                                <Box paddingLeft={3}>
+                                    <p style={{ fontSize: "15px", paddingTop: 0 }}>{PaintingType()}</p>
+                                </Box>
+                            </Paper>
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                    <Box sx={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        '& > :not(style)': {
-                        m: 4,
-                        width: 425,
-                        height: 250,
-                        },
-                    }}>
-                        <Paper elevation={3}>
-                            <Box paddingLeft={3}>
-                                <p style={{ fontSize: "22px", marginBottom: 5 }}><b>{name}</b> {year !== "" && <>(b. {year})</>}</p>
-                            </Box>
-                            <Box paddingLeft={3}>
-                                <p style={{ fontSize: "22px", paddingTop: 3, margin: 0 }}><i><b>{artwork}</b></i>, 2021</p>
-                            </Box>
-                            <Box paddingLeft={3}>
-                                <p style={{ fontSize: "15px", paddingTop: 0 }}>{PaintingType()}</p>
-                            </Box>
-                        </Paper>
-                    </Box>
-                </Grid>
-            </Grid>
+            </div>
+            <Box p={1} display="flex" justifyContent="center" alignItems="center"><Button onClick={() => handleDownloadImage()} variant="outlined">Download your art!</Button></Box>
+            <Box p={1} display="flex" justifyContent="center" alignItems="center">
+                <Button onClick={() => {
+                history.push("/");
+                setFile(null);
+            }} variant="outlined">Create more art!</Button>
+            </Box>
+            
         </Container>
     )
 }
